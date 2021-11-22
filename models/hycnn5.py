33c6bd90099653e06179ae7c46df21985037161c
HousWei <3355401301@qq.com>
2021-11-22 19:30:31 +0800

    model change

--- models/hycnn5.py
+++ models/hycnn5.py
@@ -14,18 +14,19 @@ import torch
 class HyCnn(nn.Module):
     def __init__(self, input_dim=25, num_classes=16):
         super(HyCnn, self).__init__()
-        self.tdnn1 = nn.Conv1d(input_dim, 512, kernel_size=5, padding=2)
-        self.tdnn2 = nn.Conv1d(512, 512, kernel_size=3, padding=1)
-        self.tdnn3 = nn.Conv1d(512, 512, kernel_size=3, padding=1)
-        self.tdnn4 = nn.Conv1d(512, 512, kernel_size=1, padding=0)
-        self.tdnn5 = nn.Conv1d(512, 512, kernel_size=1, padding=0)
+        self.tdnn1 = nn.Conv1d(input_dim, 128, kernel_size=5, padding=2)
+        self.tdnn2 = nn.Conv1d(128, 128, kernel_size=3, padding=1)
+        self.tdnn3 = nn.Conv1d(128, 128, kernel_size=3, padding=1)
+        self.tdnn4 = nn.Conv1d(128, 128, kernel_size=1, padding=0)
+        self.tdnn5 = nn.Conv1d(128, 128, kernel_size=1, padding=0)
+        self.bn5 = nn.BatchNorm1d(128)
         # self.tdnn1 = TDNN(input_dim=input_dim, output_dim=512, context_size=5, dilation=1, dropout_p=0.5)
         # self.tdnn2 = TDNN(input_dim=512, output_dim=512, context_size=3, dilation=1, dropout_p=0.5)
         # self.tdnn3 = TDNN(input_dim=512, output_dim=512, context_size=2, dilation=2, dropout_p=0.5)
         # self.tdnn4 = TDNN(input_dim=512, output_dim=512, context_size=1, dilation=1, dropout_p=0.5)
         # self.tdnn5 = TDNN(input_dim=512, output_dim=512, context_size=1, dilation=3, dropout_p=0.5)
         #### Frame levelPooling
-        self.segment6 = nn.Linear(512*200, 512)
+        self.segment6 = nn.Linear(128*200, 512)
         self.segment7 = nn.Linear(512, 512)
         self.output = nn.Linear(512, num_classes)
         self.softmax = nn.Softmax(dim=1)
@@ -39,6 +40,7 @@ class HyCnn(nn.Module):
         tdnn3_out = self.tdnn3(tdnn2_out)
         tdnn4_out = self.tdnn4(tdnn3_out)
         tdnn5_out = self.tdnn5(tdnn4_out)
+        tdnn5_out = self.bn5(tdnn5_out)
         tdnn5_out = tdnn5_out.view(-1,tdnn5_out.shape[1]*tdnn5_out.shape[2])
         ### Stat Pool
 
