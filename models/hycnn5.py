6c0dd6b0dba0dc9d5ef5ced92fc37841fc785ec2
HousWei <3355401301@qq.com>
2021-11-22 18:42:47 +0800

    model change

--- models/hycnn5.py
+++ models/hycnn5.py
@@ -25,7 +25,7 @@ class HyCnn(nn.Module):
         # self.tdnn4 = TDNN(input_dim=512, output_dim=512, context_size=1, dilation=1, dropout_p=0.5)
         # self.tdnn5 = TDNN(input_dim=512, output_dim=512, context_size=1, dilation=3, dropout_p=0.5)
         #### Frame levelPooling
-        self.segment6 = nn.Linear(400, 512)
+        self.segment6 = nn.Linear(512*200, 512)
         self.segment7 = nn.Linear(512, 512)
         self.output = nn.Linear(512, num_classes)
         self.softmax = nn.Softmax(dim=1)
@@ -39,12 +39,13 @@ class HyCnn(nn.Module):
         tdnn3_out = self.tdnn3(tdnn2_out)
         tdnn4_out = self.tdnn4(tdnn3_out)
         tdnn5_out = self.tdnn5(tdnn4_out)
+        tdnn5_out = tdnn5_out.view(-1,tdnn5_out.shape[1]*tdnn5_out.shape[2])
         ### Stat Pool
 
-        mean = torch.mean(tdnn5_out, 1)
-        std = torch.var(tdnn5_out, 1)
-        stat_pooling = torch.cat((mean, std), 1)
-        segment6_out = self.segment6(stat_pooling)
+        # mean = torch.mean(tdnn5_out, 1)
+        # std = torch.var(tdnn5_out, 1)
+        # stat_pooling = torch.cat((mean, std), 1)
+        segment6_out = self.segment6(tdnn5_out)
         x_vec = self.segment7(segment6_out)
         predictions = self.output(x_vec)
         return predictions
